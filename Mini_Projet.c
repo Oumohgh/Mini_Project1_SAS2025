c95d575c2c2a08b5d6ee4841f463eac5d9b333b9
oughlane <mohammedoughlane72@gmail.com>
2025-09-04 12:51:39 +0200

    add functions

--- Mini_Projet.c
+++ Mini_Projet.c
@@ -1,8 +1,33 @@
 #include<stdio.h>
+float calculerMoyenne(float notes[], int n){
+    float sum = 0;
+    for (int i = 0; i < n; i++){
+        sum = sum + notes[i];
+    }
+    return sum/n;
+}
+float trouverMax(float notes[], int n){
+    int max = notes[0];
+    for (int i = 0; i < n; i++){
+        if(notes[i] > max){
+            max = notes[i];
+        }
+    }
+    return max;
+}
+float trouverMin(float notes[], int n){
+    int min = notes[0];
+    for (int i = 0; i < n; i++){
+        if(notes[i] < min){
+            min = notes[i];
+        }
+    }
+    return min;
+}
 
 int main (){
-int nbr_etudiants,i;
-float note,sum ,moy,maxn,minn;
+int nbr_etudiants,i,nbr_reussi;
+float note,moy,maxn,minn;
 
 do
 {
@@ -23,23 +48,29 @@ for (i=0;i<nbr_etudiants;i++){
         notes[i]=note;
     }
 
-for ( i = 0; i < nbr_etudiants; i++)
-{
-    sum += notes[i];
-    
-}
-moy = sum / nbr_etudiants;
-printf("la moy est %d:",moy);
+
 for(i=0; i < nbr_etudiants;i++){
 
       if (notes[i] > maxn){
           maxn= notes[i];
-          printf("")
       }
+  else if(notes[i] <= minn){
+          maxn= notes[i];
+}
+printf("la note la plus élevée %.2f ",maxn);
+printf("note la plus faible %.2f ",minn);
+}
 }
 
 printf("\n        *******      Gestion des etudiants    ****** \n\n");
 printf("          ============================================= \n\n");
 return 0;
 
+moyenne = calculerMoyenne(students,nb_students);
+    printf("La moyenne du classe est: %.2f\n", moyenne);
+    max = trouverMax(students, nb_students);
+    printf("Le maximum est: %.2f\n", max);
+    min = trouverMin(students, nb_students);
+    printf("Le minimum est: %.2f\n", min);
+    return 0;
 }
