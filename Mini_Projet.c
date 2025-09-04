7800c7175ae83d854fcf82c7196ebc4fe8b373ef
oughlane <mohammedoughlane72@gmail.com>
2025-09-04 11:30:28 +0200

    Déclaration et saisie des données

---
+++ Mini_Projet.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main (){
+int nbr_etudiants,i;
+float note;
+
+do
+{
+printf("Combien d etudiants :");
+scanf("%d",&nbr_etudiants);
+
+}  while ( nbr_etudiants>30 ||  nbr_etudiants<=0);
+int notes[nbr_etudiants];
+
+for (i=0;i<nbr_etudiants;i++){
+        printf("Saisir la note de l'etudiant %d : ", i+1);
+        scanf("%f", &note);
+        while (note<0 || note>20){
+            printf("La note doit etre entre 0 et 20!! \n");
+            printf("Saisir la note de l'etudiant %d : ", i+1);
+            scanf("%.2f", &note);
+        }
+        notes[i]=note;
+    }
+
+
+for ( i = 0; i < nbr_etudiants; i++)
+{
+    
+}
+
+return 0;
+
+}
